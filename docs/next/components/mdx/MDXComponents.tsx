e1479a85975e9206352eb829c9da386a513ff900
benpankow <ben@elementl.com>
2022-09-21 14:30:48 -0700

    [docs][dagit-screenshot] Add CLI command to generate asset SVGs from snippets

--- docs/next/components/mdx/MDXComponents.tsx
+++ docs/next/components/mdx/MDXComponents.tsx
@@ -424,7 +424,7 @@ const Code: React.FC<CodeProps> = ({children, dagimage, ...props}) => {
       </div>
       {dagimage && (
         <RenderedDAG
-          svgSrc="/images/asset-screenshots/my_assets.svg"
+          svgSrc={'/' + dagImage}
           isAssetGraph
           mobileImgSrc="/images-2022-july/screenshots/python-assets2.png"
         />
